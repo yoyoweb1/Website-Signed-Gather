e0c36c7c3c4da4568c746b20a74937fc58605740
yoyoweb1 <85334444+yoyoweb1@users.noreply.github.com>
2021-06-10 15:20:43 +0800

    Update jiji.py

--- jiji.py
+++ jiji.py
@@ -7,7 +7,7 @@ def main(User,PassWord):
     r = requests.Session()
         # 进入官网获取cookies
         # 加入x-www-form-urlencoded头信息
-    headers = {'Host': 'ji-c.pw',
+    headers = {'Host': 'j01.best',
                 'Connection': 'keep-alive',
                 'Content-Length': '43',
                 'Accept': 'application/json, text/javascript, */*; q=0.01',
@@ -15,16 +15,16 @@ def main(User,PassWord):
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36',
                 'Sec-Fetch-Mode': 'cors',
                 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-                'Origin': 'https://ji-c.pw',
+                'Origin': 'https://j01.best',
                 'Sec-Fetch-Site': 'same-origin',
-                'Referer': 'https://ji-c.pw/signin',
+                'Referer': 'https://j01.best/signin',
                 'Accept-Encoding': 'gzip, deflate, br',
                 'Accept-Language': 'zh-CN,zh;q=0.9'
     }
         # 账号密码
     data = 'email='+User+'&passwd='+PassWord+''
         # 获取登陆后的账号
-    c = r.post(url='https://ji-c.pw/signin',data=data,headers=headers,verify=False)
+    c = r.post(url='https://j01.best/signin',data=data,headers=headers,verify=False)
         # 获取formhash
     print(chardet.detect(c.content))
     # ascii 解码
@@ -35,14 +35,14 @@ def main(User,PassWord):
                 'X-Requested-With': 'XMLHttpRequest',
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36',
                 'Sec-Fetch-Mode': 'cors',
-                'Origin': 'https://ji-c.pw',
+                'Origin': 'https://j01.best',
                 'Sec-Fetch-Site': 'same-origin',
-                'Referer': 'https://ji-c.pw/user?ran=0.21882620098942573',
+                'Referer': 'https:/j01.best/user?ran=0.21882620098942573',
                 'Accept-Encoding': 'gzip, deflate, br',
                 'Accept-Language': 'zh-CN,zh;q=0.9'
                 }
         # 签到
-    c3 = r.post(url='https://ji-c.pw/user/checkin',headers=headers1,verify=False)
+    c3 = r.post(url='https://j01.best/user/checkin',headers=headers1,verify=False)
     Content = c3.content.decode('unicode_escape')
     mail(Content=Content,Title="几鸡流量签到情况")
     r.close()
